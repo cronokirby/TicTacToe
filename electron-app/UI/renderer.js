0cc55afb151cfb334704fdb66b00df92c3071dac
cronokirby <cronokirby@gmail.com>
2016-12-31 03:43:26 +0100

    Added the electron-app folder

---
+++ electron-app/UI/renderer.js
@@ -0,0 +1,35 @@
+// Backend and endpoint details
+const host     = 'http://127.0.0.1:8080'
+const endpoint = '/hello'
+
+/*let fetchGreeting = function(url) {
+  $.getJSON(url, function(greeting) {
+      // Construct the greeting HTML output
+      let output = `${greeting.greeting}`
+
+    $('#greeting').html(output)
+  })
+}
+
+fetchGreeting(host + endpoint)*/
+function additionCallback(result){
+    let output = `${result.body}`
+    console.log(output)
+    $('#additionResult').html(output);
+}
+function fetchAddition() {
+    let postData = { body: "3 + 1" }
+    $.ajax({
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        url:      host + "/addition",
+        type:     'POST',
+        data:     JSON.stringify(postData),
+        dataType: 'json',
+        success: additionCallback
+    })
+}
+
+document.getElementById("fetchAddition").onclick = fetchAddition
